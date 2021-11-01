cc3b8e05ae885a41678e8b3f5ba4138c3047ddd9
cucarrillo <66282557+cucarrillo@users.noreply.github.com>
2021-10-31 23:24:17 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Cesar U. Carrillo', // Title for your website.
-    tagline: 'Hardworker, Fast, Software Developer',
+    tagline: 'Hardworking, Meticulous, Software Developer',
     profilePhoto: 'img/profile.png',
     url: 'https://cucarrillo.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
